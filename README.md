8fa538437385dc5cbd2a1a8730703ee7919eceb5
Jules Guesnon <jules.guesnon@hetic.net>
2024-05-06 16:35:18 +0900

    feat(.github|install.sh): Adding a release CI to have all the OS

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
-## Bum - Bun Version Manager   
+## Bum - Bun Version Manager
 
 <img src="https://github.com/owenizedd/bum/assets/26961166/b231b1ff-dcde-4cc1-a0de-fa0f4964e54e" height="auto" width="150" style="border-radius:50%">
-<br/>     
+<br/>
 
 <a href="https://github.com/owenizedd/bum/actions/workflows/rust.yml"><img alt="GitHub CI Status" src="https://img.shields.io/github/actions/workflow/status/owenizedd/bum/rust.yml?label=CI&logo=GitHub"></a> <br/>
 
+# ⚠️ This fork is only here to release ARM version of the binary
 
-Introducing Bum, a fast Bun version manager written in Rust from scratch.  
+Introducing Bum, a fast Bun version manager written in Rust from scratch.
 
 <img src=".docs/bum-quick-demo.gif" width="600">
 
 ## How to install:
 
 1. Open your terminal and execute:
+
 ```
 curl -fsSL https://github.com/owenizedd/bum/raw/main/install.sh | bash
 ```
-  > This installation will install bun for you and also bum altogether.  
-  > Bum is supported on **Linux x86_64** and **Darwin x86_64** (Mac OS)  
-  > You can enter `uname -ms` command in your terminal to see yours
-2. Restart terminal or `source ~/.zshrc` or `source ~/.bashrc` depending on your terminal.
 
+> This installation will install bun for you and also bum altogether.  
+> Bum is supported on **Linux x86_64** and **Darwin x86_64** (Mac OS)  
+> You can enter `uname -ms` command in your terminal to see yours 2. Restart terminal or `source ~/.zshrc` or `source ~/.bashrc` depending on your terminal.
 
 ## How to use:
 
@@ -32,20 +33,21 @@ curl -fsSL https://github.com/owenizedd/bum/raw/main/install.sh | bash
   - Change the current active bun version, e.g. `bum use 1.0.3`. This will automatically use v1.0.3.
   - If there's no target version is installed in the local, it will install that version then use it.
   - Else, it will just use that version directly as the active version.
-- `bum remove <version>` 
+- `bum remove <version>`
   - Remove the installed version locally.
-     > This feature will only remove local copy, but if you're using the removed version, you will still be able to use that version, but once you change to other version, you will not be able to change to that version anymore.
+    > This feature will only remove local copy, but if you're using the removed version, you will still be able to use that version, but once you change to other version, you will not be able to change to that version anymore.
   - In the future we will automatically switch to the latest version available upon removal of the version.
-- `bum list` 
+- `bum list`
   - Show all local installed versions of Bun.
-- `bum list-remote` 
+- `bum list-remote`
+
   - Show all remote versions of Bun that could be installed.
 
 - .bumrc file
   - When file exists, everytime you use `bum use` command without `<version>` argument, Bum will try to use the version from the .bumrc file.
 
 ### Future features (possibly)
-- `bum default <version>` 
 
+- `bum default <version>`
 
 > Any contribution is appreciated, have any doubts/questions/suggestions/ideas? Drop them in the Discussion page.
--- install.sh
+++ install.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! bun &> /dev/null
-then
-    curl -fsSL https://bun.sh/install | bash
+if ! bun &>/dev/null; then
+	curl -fsSL https://bun.sh/install | bash
 fi
-
 
 case $(uname -ms) in
 'Darwin x86_64')
-    target=darwin-x86_64
-    ;;
+	target=darwin-x86_64
+	;;
 'Darwin arm64')
-    target=darwin-aarch64
-    ;;
+	target=darwin-aarch64
+	;;
 'Linux aarch64' | 'Linux arm64')
-    target=linux-aarch64
-    ;;
+	target=linux-aarch64
+	;;
 'Linux x86_64' | *)
-    target=linux-x86_64
-    ;;
+	target=linux-x86_64
+	;;
 esac
 
 GITHUB=${GITHUB-"https://github.com"}
 
-github_repo="$GITHUB/owenizedd/bum"
+github_repo="$GITHUB/JulesGuesnon/bum"
 
 bum_uri=$github_repo/releases/latest/download/bum-$target
 
@@ -36,32 +34,27 @@ bin_dir=$install_dir/bin
 exe=$bin_dir/bum
 
 if [[ ! -d $bin_dir ]]; then
-    mkdir -p "$bin_dir" ||
-        error "Failed to create install directory \"$bin_dir\""
+	mkdir -p "$bin_dir" ||
+		error "Failed to create install directory \"$bin_dir\""
 fi
 
 curl --fail --location --progress-bar --output "$exe" "$bum_uri" ||
-    error "Failed to download bum from \"$bum_uri\""
-
-
-
+	error "Failed to download bum from \"$bum_uri\""
 
 chmod +x "$exe" ||
-    error 'Failed to set permissions on bum executable'
-
+	error 'Failed to set permissions on bum executable'
 
 tildify() {
-    if [[ $1 = $HOME/* ]]; then
-        local replacement=\~/
+	if [[ $1 = $HOME/* ]]; then
+		local replacement=\~/
 
-        echo "${1/$HOME\//$replacement}"
-    else
-        echo "$1"
-    fi
+		echo "${1/$HOME\//$replacement}"
+	else
+		echo "$1"
+	fi
 }
 
 echo "bum was installed successfully to  "$exe""
-
 
 refresh_command=''
 
@@ -69,123 +62,122 @@ tilde_bin_dir="$bin_dir"
 quoted_install_dir=\"${install_dir//\"/\\\"}\"
 
 if [[ $quoted_install_dir = \"$HOME/* ]]; then
-    quoted_install_dir=${quoted_install_dir/$HOME\//\$HOME/}
+	quoted_install_dir=${quoted_install_dir/$HOME\//\$HOME/}
 fi
 
 echo
 
 case $(basename "$SHELL") in
 fish)
-    commands=(
-        "set --export $install_env $quoted_install_dir"
-        "set --export PATH $bin_env \$PATH"
-    )
+	commands=(
+		"set --export $install_env $quoted_install_dir"
+		"set --export PATH $bin_env \$PATH"
+	)
 
-    fish_config=$HOME/.config/fish/config.fish
-    tilde_fish_config=$(tildify "$fish_config")
+	fish_config=$HOME/.config/fish/config.fish
+	tilde_fish_config=$(tildify "$fish_config")
 
-    if [[ -w $fish_config ]]; then
-        {
-            echo -e '\n# bum'
+	if [[ -w $fish_config ]]; then
+		{
+			echo -e '\n# bum'
 
-            for command in "${commands[@]}"; do
-                echo "$command"
-            done
-        } >>"$fish_config"
+			for command in "${commands[@]}"; do
+				echo "$command"
+			done
+		} >>"$fish_config"
 
-        info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_fish_config\""
+		info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_fish_config\""
 
-        refresh_command="source $tilde_fish_config"
-    else
-        echo "Manually add the directory to $tilde_fish_config (or similar):"
+		refresh_command="source $tilde_fish_config"
+	else
+		echo "Manually add the directory to $tilde_fish_config (or similar):"
 
-        for command in "${commands[@]}"; do
-            info_bold "  $command"
-        done
-    fi
-    ;;
+		for command in "${commands[@]}"; do
+			info_bold "  $command"
+		done
+	fi
+	;;
 zsh)
 
-    commands=(
-        "export $install_env=$quoted_install_dir"
-        "export PATH=\"$bin_env:\$PATH\""
-    )
+	commands=(
+		"export $install_env=$quoted_install_dir"
+		"export PATH=\"$bin_env:\$PATH\""
+	)
 
-    zsh_config=$HOME/.zshrc
-    tilde_zsh_config=$(tildify "$zsh_config")
+	zsh_config=$HOME/.zshrc
+	tilde_zsh_config=$(tildify "$zsh_config")
 
-    if [[ -w $zsh_config ]]; then
-        {
-            echo -e '\n# bum'
+	if [[ -w $zsh_config ]]; then
+		{
+			echo -e '\n# bum'
 
-            for command in "${commands[@]}"; do
-                echo "$command"
-            done
-        } >>"$zsh_config"
+			for command in "${commands[@]}"; do
+				echo "$command"
+			done
+		} >>"$zsh_config"
 
-        refresh_command="exec $SHELL"
-    else
-        echo "Manually add the directory to $tilde_zsh_config (or similar):"
+		refresh_command="exec $SHELL"
+	else
+		echo "Manually add the directory to $tilde_zsh_config (or similar):"
 
-        for command in "${commands[@]}"; do
-            info_bold "  $command"
-        done
-    fi
-    ;;
+		for command in "${commands[@]}"; do
+			info_bold "  $command"
+		done
+	fi
+	;;
 bash)
 
-    commands=(
-        "export $install_env=$quoted_install_dir"
-        "export PATH=$bin_env:\$PATH"
-    )
+	commands=(
+		"export $install_env=$quoted_install_dir"
+		"export PATH=$bin_env:\$PATH"
+	)
 
-    bash_configs=(
-        "$HOME/.bashrc"
-        "$HOME/.bash_profile"
-    )
+	bash_configs=(
+		"$HOME/.bashrc"
+		"$HOME/.bash_profile"
+	)
 
-    if [[ ${XDG_CONFIG_HOME:-} ]]; then
-        bash_configs+=(
-            "$XDG_CONFIG_HOME/.bash_profile"
-            "$XDG_CONFIG_HOME/.bashrc"
-            "$XDG_CONFIG_HOME/bash_profile"
-            "$XDG_CONFIG_HOME/bashrc"
-        )
-    fi
+	if [[ ${XDG_CONFIG_HOME:-} ]]; then
+		bash_configs+=(
+			"$XDG_CONFIG_HOME/.bash_profile"
+			"$XDG_CONFIG_HOME/.bashrc"
+			"$XDG_CONFIG_HOME/bash_profile"
+			"$XDG_CONFIG_HOME/bashrc"
+		)
+	fi
 
-    set_manually=true
-    for bash_config in "${bash_configs[@]}"; do
-        tilde_bash_config=$(tildify "$bash_config")
+	set_manually=true
+	for bash_config in "${bash_configs[@]}"; do
+		tilde_bash_config=$(tildify "$bash_config")
 
-        if [[ -w $bash_config ]]; then
-            {
-                echo -e '\n# bum'
+		if [[ -w $bash_config ]]; then
+			{
+				echo -e '\n# bum'
 
-                for command in "${commands[@]}"; do
-                    echo "$command"
-                done
-            } >>"$bash_config"
+				for command in "${commands[@]}"; do
+					echo "$command"
+				done
+			} >>"$bash_config"
 
-            info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_bash_config\""
+			info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_bash_config\""
 
-            refresh_command="source $bash_config"
-            set_manually=false
-            break
-        fi
-    done
+			refresh_command="source $bash_config"
+			set_manually=false
+			break
+		fi
+	done
 
-    if [[ $set_manually = true ]]; then
-        echo "Manually add the directory to $tilde_bash_config (or similar):"
+	if [[ $set_manually = true ]]; then
+		echo "Manually add the directory to $tilde_bash_config (or similar):"
 
-        for command in "${commands[@]}"; do
-            info_bold "  $command"
-        done
-    fi
-    ;;
+		for command in "${commands[@]}"; do
+			info_bold "  $command"
+		done
+	fi
+	;;
 *)
-    echo 'Manually add the directory to ~/.bashrc (or similar):'
-    info_bold "  export $install_env=$quoted_install_dir"
-    info_bold "  export PATH=\"$bin_env:\$PATH\""
-    ;;
+	echo 'Manually add the directory to ~/.bashrc (or similar):'
+	info_bold "  export $install_env=$quoted_install_dir"
+	info_bold "  export PATH=\"$bin_env:\$PATH\""
+	;;
 esac
-
